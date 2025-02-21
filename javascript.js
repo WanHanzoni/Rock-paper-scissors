58095e281516ef644f23fd322b072d1d5c62fa36
juanan1996 <54101742+juanan1996@users.noreply.github.com>
2025-02-21 05:47:33 +0100

    Pushing basic game not ending

--- javascript.js
+++ javascript.js
@@ -1,4 +1,42 @@
-function add7(number) {
-    return (number + 7);
+function getComputerChoice() {
+    return Math.random();
 }
-console.log(add7(2));
+
+
+function getHumanChoice() {
+    return prompt().toLocaleLowerCase();
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+let humanScore = 0
+let computerScore = 0
+
+
+function playRound(humanChoice, computerChoice) {
+
+    computerChoice = computerSelection * 10 % 3
+
+    if (computerChoice <= 1) {
+        computerChoice = "rock"
+    }
+    if (computerChoice <= 2) {
+        computerChoice = "paper"
+    }
+    if (computerChoice <= 3) {
+        computerChoice = "scissors"
+    }
+
+    console.log("Human plays: " + humanSelection)
+    console.log("Computer plays: " + computerChoice)
+
+    if (computerChoice == "rock" && humanChoice == "scissors") {
+        computerScore + 1;
+        // playRound();
+    }
+}
+
+playRound(humanSelection, computerSelection)
+
+
