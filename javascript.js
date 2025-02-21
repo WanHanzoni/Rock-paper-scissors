e1136aa8b454798b2b7e3e6a5c578fba0f36b3ad
juanan1996 <54101742+juanan1996@users.noreply.github.com>
2025-02-21 10:17:04 +0100

    Rock Paper Scissors game funcional

--- javascript.js
+++ javascript.js
@@ -23,8 +23,12 @@ function getHumanChoice() {
 let humanScore = 0
 let computerScore = 0
 
+if (computerScore == 5 || humanScore == 5) {
+
+}
 
 function playRound() {
+
     var humanSelection = getHumanChoice()
     var computerSelection = getComputerChoice()
 
@@ -34,14 +38,61 @@ function playRound() {
     if (computerSelection == humanSelection) {
         // playRound(humanSelection, computerSelection);
         alert("Empate");
-        playRound();
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
     }
 
     if (computerSelection == "scissors" && humanSelection == "rock") {
         humanScore = humanScore + 1;
         alert("Un punto para ti Crack! " + humanScore);
-        playRound();
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
     }
+    if (computerSelection == "rock" && humanSelection == "paper") {
+        humanScore = humanScore + 1;
+        alert("Un punto para ti Crack! " + humanScore);
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
+    }
+    if (computerSelection == "paper" && humanSelection == "scissors") {
+        humanScore = humanScore + 1;
+        alert("Un punto para ti Crack! " + humanScore);
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
+    }
+    if (computerSelection == "rock" && humanSelection == "scissors") {
+        computerScore = computerScore + 1;
+        alert("Te va a ganar una máquina Manco! " + computerScore);
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
+    }
+    if (computerSelection == "paper" && humanSelection == "rock") {
+        computerScore = computerScore + 1;
+        alert("Te va a ganar una máquina Manco! " + computerScore);
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
+    }
+    if (computerSelection == "scissors" && humanSelection == "paper") {
+        computerScore = computerScore + 1;
+        alert("Te va a ganar una máquina Manco! " + computerScore);
+        if (humanScore == 5 || computerScore == 5) {
+            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
+        }
+        else playRound();
+    }
+
 }
 
 playRound();
