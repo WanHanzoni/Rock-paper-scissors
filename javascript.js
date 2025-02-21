7ee2719f6589d5a89db67e59205880106396ea00
juanan1996 <54101742+juanan1996@users.noreply.github.com>
2025-02-21 11:27:12 +0100

    Optimizing the javascript code for victories

--- javascript.js
+++ javascript.js
@@ -1,100 +1,46 @@
+let hand = ["rock", "paper", "scissors"]
+
 function getComputerChoice() {
     var computerChoice = Math.random() * 10 % 3;
-
-    if (computerChoice <= 1) {
-        computerChoice = "rock"
-    }
-    if (computerChoice <= 2) {
-        computerChoice = "paper"
-    }
-    if (computerChoice <= 3) {
-        computerChoice = "scissors"
-    }
-    return computerChoice;
+    return computerChoice.toString()[0];
 }
-
 
 function getHumanChoice() {
-    var humanChoice = prompt().toLocaleLowerCase();
-    return humanChoice;
+    var humanChoice = prompt("Choose Rock, Paper or Scissors").toLocaleLowerCase();
+    return hand.indexOf(humanChoice);
 }
-
 
 let humanScore = 0
 let computerScore = 0
-
-if (computerScore == 5 || humanScore == 5) {
-
-}
 
 function playRound() {
 
     var humanSelection = getHumanChoice()
     var computerSelection = getComputerChoice()
 
-    console.log("Human plays: " + humanSelection);
-    console.log("Computer plays: " + computerSelection);
+    console.log("Human plays: " + hand[humanSelection]);
+    console.log("Computer plays: " + hand[computerSelection] + computerSelection);
 
-    if (computerSelection == humanSelection) {
-        // playRound(humanSelection, computerSelection);
-        alert("Empate");
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
+    if (humanSelection - computerSelection == -2 || humanSelection - computerSelection == 1) {
+        humanScore = humanScore + 1
+        alert("Un punto para ti Crack!\nYo: " + humanScore + "\nMáquina: " + computerScore)
+        final()
+    } else if (humanSelection - computerSelection == 2 || humanSelection - computerSelection == -1) {
+        computerScore = computerScore + 1
+        alert("Te va a ganar una máquina Manco!\nYo: " + humanScore + "\nMáquina: " + computerScore)
+        final()
+    } else if (humanSelection == computerSelection) {
+        alert("Empate\nYo: " + humanScore + "\nMáquina: " + computerScore);
+        final()
     }
+}
 
-    if (computerSelection == "scissors" && humanSelection == "rock") {
-        humanScore = humanScore + 1;
-        alert("Un punto para ti Crack! " + humanScore);
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
+function final() {
+    if (humanScore == 5 || computerScore == 5) {
+        alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\nCrack: " + humanScore);
+    } else {
+        playRound();
     }
-    if (computerSelection == "rock" && humanSelection == "paper") {
-        humanScore = humanScore + 1;
-        alert("Un punto para ti Crack! " + humanScore);
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
-    }
-    if (computerSelection == "paper" && humanSelection == "scissors") {
-        humanScore = humanScore + 1;
-        alert("Un punto para ti Crack! " + humanScore);
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
-    }
-    if (computerSelection == "rock" && humanSelection == "scissors") {
-        computerScore = computerScore + 1;
-        alert("Te va a ganar una máquina Manco! " + computerScore);
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
-    }
-    if (computerSelection == "paper" && humanSelection == "rock") {
-        computerScore = computerScore + 1;
-        alert("Te va a ganar una máquina Manco! " + computerScore);
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
-    }
-    if (computerSelection == "scissors" && humanSelection == "paper") {
-        computerScore = computerScore + 1;
-        alert("Te va a ganar una máquina Manco! " + computerScore);
-        if (humanScore == 5 || computerScore == 5) {
-            alert("Se acabó la partida... \nPuntuación: \n" + "Máquina: " + computerScore + "\n Crack: " + humanScore);
-        }
-        else playRound();
-    }
-
 }
 
 playRound();
-
-
--- testmath.js
+++ testmath.js
@@ -1,20 +1,11 @@
-function solicitarDato() {
-    let dato = prompt("Ingresa un número mayor que 10:");
+let hand = ["rock", "paper", "scissors"]
 
-    if (dato === null) {
-        // Si el usuario cancela el prompt, salir de la función
-        alert("Operación cancelada.");
-        return;
-    }
-
-    dato = parseInt(dato, 10); // Convertimos el dato a número
-
-    if (isNaN(dato) || dato <= 10) {
-        alert("Dato inválido. Inténtalo de nuevo.");
-        solicitarDato(); // Vuelve a llamar a la función si la condición no se cumple
-    } else {
-        alert("Dato válido ingresado: " + dato);
-    }
+function getComputerChoice() {
+    var computerChoice = Math.random() * 10 % 3;
+    return computerChoice
 }
 
-solicitarDato(); // Llamamos la función inicialmente
+var computerSelection = getComputerChoice()
+console.log(computerSelection);
+
+console.log(computerSelection.toString()[0]);
